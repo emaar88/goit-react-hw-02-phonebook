ba794db3aa37061b4ed248411424a1400e3d1081
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2020-08-09 03:02:33 +0300

    Updates

---
+++ precache-manifest.ee4add1cfe4f681d1dac10071caaef26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7aedcc5c794a77215ad65ddee869ec",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "bbc725fcf1bedf079296",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dea4d0ce7e5cb1e5e29e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.690255fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.690255fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc725fcf1bedf079296",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.e498c429.chunk.js"
+  },
+  {
+    "revision": "50fa581c508d2a3f3269",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.405e8705.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goit-react-hw-02-phonebook/static/media/logo.5d5d9eef.svg"
+  }
+]);
